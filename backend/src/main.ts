3330b27e749f0b71c547d3d84bef81a26be2df8f
Yogyaa <yogyaa.varma@gmail.com>
2025-02-11 02:23:45 +0530

    deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as express from 'express';
-import { join } from 'path';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as express from "express";
+import { join } from "path";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200'], // Allowed origins
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
+    origin: ["https://ecomadminconsole-front.onrender.com/"], // Allowed origins
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed methods
     credentials: true, // Allow credentials (e.g., cookies)
-    allowedHeaders: 'Content-Type, Accept', // Allowed headers
+    allowedHeaders: "Content-Type, Accept", // Allowed headers
   });
-  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+  app.use("/uploads", express.static(join(__dirname, "..", "uploads")));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
